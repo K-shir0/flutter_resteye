70d47003b0acc989ebaaac233bbfde343f114e82
K-shir0 <kakishiro2190394@gmail.com>
2022-10-07 09:54:20 +0900

    update: ファイルを分割

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_resteye/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = PinPController();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('RestEyes')),
+      body: Column(
+        children: [
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 300),
+            child: const PinPView(),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => controller.toggle(),
+        tooltip: 'PinP',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+/// PinP を操作するためのコントローラ
+class PinPController {
+  PinPController();
+
+  final _channel =
+      const MethodChannel('jp.kshiro.resteye.flutterResteye/sample');
+
+  Future<void> toggle() async {
+    await _channel.invokeMethod('toggle');
+  }
+}
+
+/// PinP を表示できる View
+class PinPView extends StatelessWidget {
+  const PinPView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+      transform: Matrix4.translationValues(190, 30, 0),
+      child: const UiKitView(viewType: 'sample'),
+    );
+  }
+}
