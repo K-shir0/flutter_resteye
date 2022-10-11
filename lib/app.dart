91f2ecdc42e5469b9cc5e7534bc98100f2d69bc1
K-shir0 <kakishiro2190394@gmail.com>
2022-10-11 10:25:14 +0900

    update: ScreenUtil を導入

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_resteye/pages/home_page.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(fontFamily: 'NotoSansJP'),
-      home: const HomePage(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(fontFamily: 'NotoSansJP'),
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
