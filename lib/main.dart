1083db7ea60f92e113ca29fb90d1f8034500e24b
toasahi <asa1313to@gmail.com>
2022-10-12 14:14:48 +0900

    update:Splashページ作成

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'app.dart';
 
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
   runApp(const App());
 }
