3c2db8164341b4bc52f1c9fe4f00d0df4f07b400
K-shir0 <kakishiro2190394@gmail.com>
2022-11-28 14:49:17 +0900

    update: 2日以上だとリセットする処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,21 @@ void main() async {
 
   // 最終使用時間を取得
   // 初期値(0) でないかつ差が1日以上ある場合は使用回数を増加
+  // 2日以上であれば使用回数をリセット
   final lastUsedTime = await getLastUsedTime();
+  // 何日ぶりに起動するかの日数
+  final diffUsedDays = DateTime.now().difference(lastUsedTime).inDays;
+
   if (lastUsedTime != DateTime.fromMillisecondsSinceEpoch(0) &&
-      DateTime.now().difference(lastUsedTime).inDays >= 1) {
-    incrementNumberOfTimesUsed();
+      diffUsedDays >= 1) {
+    // 2日以上か判定
+    if (diffUsedDays >= 2) {
+      // 2日以上の場合は使用回数を0にする
+      resetNumberOfTimesUsed();
+    } else {
+      // 1日以上の場合は使用回数を1増加
+      incrementNumberOfTimesUsed();
+    }
   }
   // 最終使用時間を更新
   saveLastUsedTime();
