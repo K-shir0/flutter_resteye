3ef8ad6df9176d1f94907d34b4a8425ca750773e
K-shir0 <50326556+K-shir0@users.noreply.github.com>
2023-01-25 14:03:58 +0900

    update: att 対応 (#65)

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -11,8 +11,22 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:page_transition/page_transition.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // 広告のトラッキング許可
+    // 最初に表示した方が UI/UX 的によさそう
+    RestEyeATT.confirmATTStatus();
+  }
 
   @override
   Widget build(BuildContext context) {
